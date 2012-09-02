d9fbf2137a3b75b1e536506702dfd17d7b7611a0
Tony Majestro <tonymajestro@gmail.com>
2012-09-02 17:15:39 -0400

    Crypto stuff

--- Python/Cryptography/Homework/Assignment1/problem5/p5.py
+++ Python/Cryptography/Homework/Assignment1/problem5/p5.py
@@ -1,33 +1,28 @@
 import frequency
 
-
-
-
-
-def solve(word, wordMonograms, popMonograms, i, j):
-    if len(wordMonograms) == 0:
-        print word
+def solve(word, mg, fmg):
+    if (len(mg) == 0 or len(fmg) == 0):
+        #print word
         return
-    if len(popMonograms) == 0:
-        print word
-        return
+        
+    a = mg[0]
+    b = fmg[0]
+    print ("{0}, {1}".format(a,b))
     
-    newWord = word.replace(wordMonograms[i], popMonograms[j].upper())
-    solve(newWord, wordMonograms[1:], popMonograms[1:])
-    solve(newWord, wordMonograms, popMonograms[1:])
-    solve(newWord, wordMonograms[1:], popMonograms)
+    newWord = word.replace(a, b.upper())
+    solve(newWord, mg[1:], fmg[1:])
+    solve(word, mg, fmg[1:] + [fmg[0]])
     
+    
+
+
 
 
                 
 if (__name__ == "__main__"):
-    #word = open('word.txt').read().rstrip()
-    word = 'aaaabbbbbbcc'
-    monograms = frequency.monograms[:2]
-    wordMonograms = frequency.getMonograms(word)[:2]
-    solve(word, wordMonograms, monograms, 0, 0)
-            
-            
-        
-
+    
+    word = open('word.txt').read().rstrip()
+    monographs = frequency.getMonograms(word)[:2]
+    frequentMonographs = frequency.monograms[:2]
+    solve(word, monographs, frequentMonographs)
     
