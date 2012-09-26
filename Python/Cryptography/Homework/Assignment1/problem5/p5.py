424f178bafa425ce293afa46deb57de7ae6450af
Tony Majestro <tonymajestro@gmail.com>
2012-09-26 02:24:54 -0400

    Added ps5 in crypto

--- Python/Cryptography/Homework/Assignment1/problem5/p5.py
+++ Python/Cryptography/Homework/Assignment1/problem5/p5.py
@@ -1,27 +1,41 @@
-import frequency
+import collections
+import re
+
+message = open('encryptedMessage.txt').read().rstrip()
+
+# Get frequency of each letter
+letters = collections.Counter(message)
+
+# Get frequency of each digraph
+digraphs = collections.Counter(re.findall(r'(?=(..))', message))
+
+# Get frequency of each trigraph
+trigraphs = collections.Counter(re.findall(r'(?=(...))', message))
+
+# Print out letter frequencies
+#print
+#print letters
+#print
+#print digraphs
+#print
+#print
+#print trigraphs
+#print
+
+d = {'Z':'e', 'G':'r', 'K':'t', 'O':'w', 'J':'h', 'P':'a'}
+
+def decrypt():
+    ls = []
+    for c in message:
+        if d.has_key(c):
+            ls.append(d[c])
+        else:
+            ls.append(c)
+    return ''.join(ls)
+
+decrypt()
 
 
-def sub(d, a):
-    if (d.has_key(a)):
-        return d[a].upper()
-    return a
 
-
-def tryTrigraph(w, a, b):
-    d = {}
-    for i,c in enumerate(a):
-        d[c] = b[i]
-        
-    if len(d) != len(a):
-        return ''
-    
-    return a
-
-
-if (__name__ == "__main__"):
-    word = open('word.txt').read().rstrip()
-    
-    for c in frequency.getTrigrams(word):
-        s = tryTrigraph(word, c, 'the')
-        if len(s) > 0 and (s[2] == 'z'):
-            print s
+if __name__ == '__main__':
+    print decrypt()
--- Python/Cryptography/Homework/Assignment3/p1.py
+++ Python/Cryptography/Homework/Assignment3/p1.py
@@ -6,38 +6,50 @@ def generateKeyStream(initVector, recurrence):
         yield registers.pop(0)
     
 
-def getKeyPeriod(initVector, recurrence):
-    registers = list(initVector)
+# seed - the initial state for the LFSR
+def getKeyPeriod(seed):
+    # model registers as a list of numbers initialized to the seed
+    registers = list(seed)
     period = 0
-    while (period == 0) or (registers != initVector):
-        registers.append(recurrence(registers))
+    # loop until the register state is back to the original seed
+    while (period == 0) or (registers != seed):
+        # add the new value to the end of the register list and
+        # remove the first register value
+        registers.append(registers[0] ^ registers[1])
         registers.pop(0)
         period += 1
     return period
 
 
-
-
-
-        
+# recurrence relation for problem 1 is to add the first two values
+# mod 2.  This is effectively performing an XOR of of the values        
 def recurrence(key):
     return key[0] ^ key[1]
 
     
-
+# Returns all the permutations up to but not including num as a 
+# list of binary values.
+# Each binary value is represented as a list of integers, 0 or 1
 def getPermutations(num):
-    permStrings = ['{0:05b}'.format(i) for i in range(num)]
     perms = []
-    for perm in permStrings:
-        perms.append([int(c) for c in perm])
+    for i in range(num):
+        # get ith permutation as a binary string padded with 5 zeroes
+        # and convert to a list of integers
+        permStr = '{0:05b}'.format(i)
+        perm = [int(x) for x in permStr]
+        perms.append(perm)
+        
     return perms
 
 
 
     
 if __name__ == '__main__':
-    for perm in getPermutations(32):
-        print perm, getKeyPeriod(perm, recurrence)
+    
+    # print all 32 seeds and their periods for the LFSR
+    for seed in getPermutations(32):
+        period = getKeyPeriod(seed)
+        print '{0:2s}\t{1:3d}'.format(''.join([str(x) for x in seed]), period)
     
     
     
